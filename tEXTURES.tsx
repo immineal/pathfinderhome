f304f89df8b3bb1e50622a4e72cb72e9c59aec74
Linus Linhof <linus@shaun.lisa.loc>
2021-01-31 14:54:59 +0100

    tEXTURES.tsx: custom textures

---
+++ tEXTURES.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tEXTURES" tilewidth="29" tileheight="29" tilecount="9" columns="3">
+ <image source="../tEXTURES.jpg" trans="3383fe" width="87" height="87"/>
+</tileset>
